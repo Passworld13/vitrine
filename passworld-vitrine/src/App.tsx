90b4447906eeea098b2981383af9a6e9d963a001
zekri <marcoz@MacBook-Pro-de-zekri.local>
2025-04-08 18:43:23 +0200

    Clean App.tsx - site vitrine simple

---
+++ passworld-vitrine/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <img src="/psw3.png" alt="Passworld Logo" className="logo" />
+      <h1 className="title">Passworld</h1>
+      <p className="subtitle">Site under construction. Powered by BONK on Telegram.</p>
+    </div>
+  )
+}
+
+export default App
