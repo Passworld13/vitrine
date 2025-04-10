d96ec6198ac1244472e8c1a773795d64565be55e
zekri <marcoz@MacBook-Pro-de-zekri.local>
2025-04-10 15:03:16 +0200

    Fix: Register blueprints after app creation

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 from flask import Flask
 from admin_route import admin_bp
+from guess_route import guess_bp
 
 app = Flask(__name__)
+
+# Register routes *after* app is defined
 app.register_blueprint(admin_bp)
+app.register_blueprint(guess_bp)
 
 @app.route("/")
 def index():
-    return "Welcome to API Passworld v2 🧠🔐"
+    return "Welcome to the Passworld API 🧠🔐"
 
 if __name__ == "__main__":
     app.run(debug=True)
+
 
 
